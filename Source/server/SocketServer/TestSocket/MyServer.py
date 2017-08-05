6d8d1f5a88d2fb2226707f87bf9304e5fd32bb49
willy2358 <willy2358@139.com>
2017-08-05 23:49:03 +0800

    Added basic logic in socket server to process player commands

--- Source/server/SocketServer/TestSocket/MyServer.py
+++ Source/server/SocketServer/TestSocket/MyServer.py
@@ -1,44 +1,64 @@
 import socketserver
 
+import Player
+
+waiting_players = {}
+
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
+    __Players = []
+
     def handle(self):
         conn = self.request
+        player = Player.Player(conn)
+        MyTCPHandler.__Players.append({conn, player})
+        print('client:' + str(len(MyTCPHandler.__Players)))
+        print(MyTCPHandler.__Players)
+
         print(self.client_address)
-        #conn.sendall('I am multi thread')
-        Flag = True
-        while Flag:
+
+        flag = True
+        while flag:
             data = conn.recv(1024)
+            self.dispatch_player_commands(conn, data);
             print('received:' + data.decode())
-            if data == 'exit':
-                Flag = False
-            else:
-                #bdata = bytes(str(data), encoding="utf-8")
-		#bdata = bytes(data,encode='utf-8')
-                #conn.sendall('input:' + bdata)
-                resp = 'your input :' + data.decode()
-                print('response:' + resp)
-                try:
-                    conn.sendall(resp.encode(encoding="utf-8"))
-                except Exception as x:
-                    print('Send exception:' + x.message)
-                        
-                
-                
-        
-    """
-    The request handler class for our server.
+            # print(conn)
+            # print(MyTCPHandler.__Clients.index(conn))
+            # if data == 'exit':
+            #     flag = False
+            # else:
+            #     resp = 'your input :' + data.decode()
+            #     print('response:' + resp)
+            #     try:
+            #         conn.sendall(resp.encode(encoding="utf-8"))
+            #     except Exception as x:
+            #         print('Send exception:' + x.message)
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+    def dispatch_player_commands(self, conn, comm_text):
+        parts = comm_text.split('#')
+        if len(parts) == 2:
+            if parts[0].lower() == "play_rule":
+                self.process_command_play_rule(conn, comm_text)
+            if parts[0].lower() == "play_card":
+                self.process_command_play_card(conn, comm_text)
+            if parts[0].lower() == "play_leave":
+                self.process_command_play_leave(conn)
 
-    
+    def process_command_play_rule(self, command_text):
+        pass
+
+    def process_command_play_card(self, conn, command_text):
+        pass
+
+    def process_command_play_leave(self, conn):
+        pass
+
+
 if __name__ == "__main__":
     HOST, PORT = "127.0.0.1", 9229
 
     # Create the server, binding to localhost on port 9999
-    #server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    # server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
     server = socketserver.ThreadingTCPServer((HOST,PORT), MyTCPHandler)
 
     # Activate the server; this will keep running until you
