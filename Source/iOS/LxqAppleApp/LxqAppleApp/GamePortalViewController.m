20ac70a4ee38626448fada1900d2daaa40110ae3
willy2358 <willy2358@139.com>
2018-02-10 19:13:42 +0800

    Updated the support for socket

--- Source/iOS/LxqAppleApp/LxqAppleApp/GamePortalViewController.m
+++ Source/iOS/LxqAppleApp/LxqAppleApp/GamePortalViewController.m
@@ -6,9 +6,9 @@
 //  Copyright © 2018年 metalight. All rights reserved.
 //
 
-@import CocoaAsyncSocket;
+//@import CocoaAsyncSocket;
 #import "GamePortalViewController.h"
-//#import "GCDAsyncSocket.h"
+#import "GCDAsyncSocket.h"
 
 @interface GamePortalViewController () <GCDAsyncSocketDelegate>
 
@@ -21,6 +21,7 @@
 {
     //    NSString * serverIP;
     //    NSInteger serverPort;
+    GCDAsyncSocket *_socket;
 }
 
 
@@ -43,16 +44,25 @@
     //    NSString *host = @"133.33.33.1";
     //    int port = 1212;
     
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    
+    _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:mainQueue];
     //创建一个socket对象
-    GCDAsyncSocket *_socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+    
+//    GCDAsyncSocket *_socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
     
     //连接
     NSError *error = nil;
-    [_socket connectToHost:self.serverIP onPort:self.serverPort error:&error];
-    
-    if (error) {
+    if(![_socket connectToHost:self.serverIP onPort:self.serverPort error:&error]){
         NSLog(@"%@",error);
     }
+    else{
+        
+    }
+    
+//    if (error) {
+//        NSLog(@"%@",error);
+//    }
     
 }
 
@@ -93,6 +103,65 @@
 -(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
     NSString *receiverStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"%s %@",__func__,receiverStr);
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+{
+//    DDLogInfo(@"socket:%p didConnectToHost:%@ port:%hu", sock, host, port);
+    
+    //    DDLogInfo(@"localHost :%@ port:%hu", [sock localHost], [sock localPort]);
+    
+#if USE_SECURE_CONNECTION
+    {
+        // Connected to secure server (HTTPS)
+        
+        // Configure SSL/TLS settings
+        NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithCapacity:3];
+        
+        // If you simply want to ensure that the remote host's certificate is valid,
+        // then you can use an empty dictionary.
+        
+        // If you know the name of the remote host, then you should specify the name here.
+        //
+        // NOTE:
+        // You should understand the security implications if you do not specify the peer name.
+        // Please see the documentation for the startTLS method in GCDAsyncSocket.h for a full discussion.
+        
+        [settings setObject:@"www.paypal.com"
+                     forKey:(NSString *)kCFStreamSSLPeerName];
+        
+        // To connect to a test server, with a self-signed certificate, use settings similar to this:
+        
+        //    // Allow expired certificates
+        //    [settings setObject:[NSNumber numberWithBool:YES]
+        //                 forKey:(NSString *)kCFStreamSSLAllowsExpiredCertificates];
+        //
+        //    // Allow self-signed certificates
+        //    [settings setObject:[NSNumber numberWithBool:YES]
+        //                 forKey:(NSString *)kCFStreamSSLAllowsAnyRoot];
+        //
+        //    // In fact, don't even validate the certificate chain
+        //    [settings setObject:[NSNumber numberWithBool:NO]
+        //                 forKey:(NSString *)kCFStreamSSLValidatesCertificateChain];
+        
+        DDLogInfo(@"Starting TLS with settings:\n%@", settings);
+        
+        [sock startTLS:settings];
+        
+        // You can also pass nil to the startTLS method, which is the same as passing an empty dictionary.
+        // Again, you should understand the security implications of doing so.
+        // Please see the documentation for the startTLS method in GCDAsyncSocket.h for a full discussion.
+        
+    }
+#else
+    {
+        // Connected to normal server (HTTP)
+        //        NSData *data = [[NSData alloc] init];
+        NSString *aString = @"1234abcd";
+        NSData *aData = [aString dataUsingEncoding: NSUTF8StringEncoding];
+        [sock writeData:aData withTimeout:5 tag:2];
+    }
+#endif
 }
 
 - (IBAction)enterRoom_Clicked:(id)sender {
