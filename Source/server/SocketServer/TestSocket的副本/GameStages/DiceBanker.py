a4e5ad6ee999c5f0ecda4b424e7a102dc0e14f6e
willy2358 <willy2358@139.com>
2018-10-03 22:22:40 +0800

    Deleted TestSocket-copy

--- Source/server/SocketServer/TestSocket的副本/GameStages/DiceBanker.py
+++
@@ -1,6 +0,0 @@
-from GameStages.GameStage import GameStage
-
-
-class DiceBanker(GameStage):
-    def __init__(self, rule):
-        super(DiceBanker, self).__init__(rule)
--- Source/server/SocketServer/TestSocket的副本/Utils.py
+++
@@ -1,8 +0,0 @@
-
-
-def list_remove_parts(src_list, parts):
-    for i in parts:
-        if i in src_list:
-            src_list.remove(i)
-
-    return src_list
