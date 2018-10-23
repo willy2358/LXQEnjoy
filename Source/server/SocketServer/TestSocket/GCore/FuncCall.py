c37545086d006e028773b482d77f4e03b2483e18
willy2358 <willy2358@139.com>
2018-10-23 13:15:11 +0800

    Updated to set xml node for parsed Operands

--- Source/server/SocketServer/TestSocket/GCore/FuncCall.py
+++ Source/server/SocketServer/TestSocket/GCore/FuncCall.py
@@ -1,5 +1,7 @@
 
-class FuncCall:
+from GCore.Operand import Operand
+
+class FuncCall(Operand):
     def __init__(self, funcName):
         self.__func_name = funcName
         self.__arguments = []
