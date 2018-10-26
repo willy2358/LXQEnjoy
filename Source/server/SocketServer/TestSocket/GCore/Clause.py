bd5f1805e0e058f33060e79363c2776958d4e44f
willy2358 <willy2358@139.com>
2018-10-26 23:14:26 +0800

    Debug through loop node in running

--- Source/server/SocketServer/TestSocket/GCore/Clause.py
+++ Source/server/SocketServer/TestSocket/GCore/Clause.py
@@ -5,28 +5,35 @@ from GCore.Statement import Statement
 class Clause(Statement):
     def __init__(self, Op = Operator.And):
         self.__case = None
-        self.__true_updates = []
-        self.__false_updates = []
+        self.__true_statements = []
+        self.__false_statements = []
+
+    def get_true_statements(self):
+        return self.__true_statements
+
+    def get_false_statements(self):
+        return self.__false_statements
 
     def set_case(self, case):
         if case:
             self.__case = case
 
-    def add_true_update(self, update):
-        self.__true_updates.append(update)
+    def add_true_statement(self, update):
+        self.__true_statements.append(update)
 
-    def add_false_update(self, update):
-        self.__false_updates.append(update)
+    def add_false_statement(self, update):
+        self.__false_statements.append(update)
 
-    def set_true_updates(self, updates):
-        self.__true_updates = updates
+    def set_true_statements(self, updates):
+        self.__true_statements = updates
 
-    def set_false_updates(self, updates):
-        self.__false_updates = updates
+    def set_false_statements(self, updates):
+        self.__false_statements = updates
+
 
     def execute(self):
         if self.__case is not None and self.__case.is_satisfy():
-            for u in self.__true_updates:
+            for u in self.__true_statements:
                 u.execute()
 
 
--- Source/server/SocketServer/TestSocket/GCore/Update.py
+++ Source/server/SocketServer/TestSocket/GCore/Update.py
@@ -5,25 +5,26 @@ from GCore.Engine import *
 import GCore.Engine
 
 class Update(Statement):
-    def __init__(self, targetObj, targetProp, opVal, op = Operator.Update):
-        self.__targetObj = targetObj
-        self.__targetProp = targetProp
-        self.__op = op
-        self.__opVal = opVal
-        self.__case = None
+    # def __init__(self, target, targetProp, opVal, op = Operator.Update):
+    #     self.__target = target
+    #     # self.__targetProp = targetProp
+    #     self.__op = op
+    #     self.__opVal = opVal
+    #     self.__case = None
 
     def __init__(self, target, opVal, op = Operator.Update):
         if not GCore.Engine.is_var_ref(target):
             raise Exception("should be a variable, start with @")
-        ps = target.split('.')
-        assert len(ps) >= 2, "at least two parts, seperated by dot"
-        self.__targetProp = ps[-1]
-        self.__targetObj = target[0:-(len(self.__targetProp) + 1)]
+        # ps = target.split('.')
+        # assert len(ps) >= 2, "at least two parts, seperated by dot"
+        self.__target = target
+        # self.__targetObj = target[0:-(len(self.__targetProp) + 1)]
         self.__op = op
         self.__opVal = opVal
+        self.__case = None
 
     def get_target_property(self):
-        return self.__targetProp
+        return self.__target
 
     def execute(self):
         pass
